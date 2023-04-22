8c07db72818efd7e5d0a540eea59c61d23b55e8d
misu <daum091409@naver.com>
2023-04-22 19:01:28 +0900

    04/22

--- src/Bike_Categori/Bike_Categori.js
+++ src/Bike_Categori/Bike_Categori.js
@@ -6,6 +6,11 @@ import Battery from '../assets/material-symbols_battery-full-alt.svg'
 import plug from '../assets/material-symbols_power-plug.svg'
 import gogo from '../assets/material-symbols_speed.svg'
 import water from '../assets/mdi_water-off.svg'
+import Bike from '../assets/bike.svg'
+
+import { AiOutlineRight } from "react-icons/ai";
+
+//AiOutlineRight
 const Bike_Categori = function () {
 
 
@@ -15,9 +20,9 @@ const Bike_Categori = function () {
   <>
    <div style={{
     width: '100 %',
-    height: '45vh',
+    height: '50vh',
 
-    position: 'relative'
+    position: 'relative',
 
    }}>
     <img
@@ -52,7 +57,7 @@ const Bike_Categori = function () {
    }}>
     <p style={{
      fontWeight: 'bold',
-     fontSize: '30px'
+     fontSize: '35px'
     }}>
      Himo High -Quality E-bike
     </p>
@@ -63,7 +68,8 @@ const Bike_Categori = function () {
     height: '50vh',
 
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginTop: '10px'
    }}>
     <div style={{
      width: '80%',
@@ -74,15 +80,15 @@ const Bike_Categori = function () {
     }}>
 
      <div style={{
-      width: '25%',
+      width: '40%',
       height: '100%',
-      backgroundColor: 'pink',
+
       display: 'flex',
       flexDirection: 'column'
      }}>
       <p style={{
        fontWeight: 'bold',
-       fontSize: '25px',
+       fontSize: '30px',
        margin: '5px'
       }}>
        20inch Folding
@@ -90,13 +96,13 @@ const Bike_Categori = function () {
       </p>
       <p style={{
        fontWeight: 'bold',
-       fontSize: '25px',
+       fontSize: '30px',
        margin: '5px'
       }}>
        EBike
       </p>
       <p style={{
-       fontSize: '13px',
+       fontSize: '15px',
        fontWeight: '600'
       }}>
        HIMO Cost-Effective E-bike
@@ -114,7 +120,7 @@ const Bike_Categori = function () {
          marginBottom: 'auto',
          fontWeight: '600',
 
-         fontSize: '13px',
+         fontSize: '15px',
 
         }}
 
@@ -137,7 +143,7 @@ const Bike_Categori = function () {
         height: '50%',
         background: 'white',
         display: 'flex',
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
         alignItems: 'center'
        }}>
         <div style={{
@@ -146,23 +152,31 @@ const Bike_Categori = function () {
 
          display: 'flex',
          justifyContent: 'center',
-         flexDirection: 'column'
+         flexDirection: 'column',
+
         }}>
-         <img
-          style={{
-           width: '50%',
-           height: '50%',
-           objectFit: 'cover'
+         <div style={{
+          width: '50%',
+          height: '50%'
+         }}>
 
 
-          }}
-          src={Battery}
-          title='good'
-         />
+          <img
+           style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover'
+
+
+           }}
+           src={Battery}
+           title='good'
+          />
+         </div>
          <p
           style={{
            margin: '5px',
-           fontSize: '10px',
+           fontSize: '13px',
            fontWeight: '500'
 
           }}
@@ -171,7 +185,7 @@ const Bike_Categori = function () {
          </p>
          <p style={{
           margin: '5px',
-          fontSize: '10px',
+          fontSize: '13px',
           fontWeight: '500'
 
          }}>
@@ -189,21 +203,27 @@ const Bike_Categori = function () {
          flexDirection: 'column'
 
         }}>
-         <img
-          style={{
-           width: '50%',
-           height: '50%',
-           objectFit: 'cover'
+         <div style={{
+          width: '50%',
+          height: '50%'
+         }}>
+
+          <img
+           style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover'
 
 
-          }}
-          src={plug}
-          title='good'
-         />
+           }}
+           src={plug}
+           title='good'
+          />
+         </div>
          <p
           style={{
            margin: '5px',
-           fontSize: '10px',
+           fontSize: '13px',
            fontWeight: '500'
 
           }}
@@ -215,7 +235,7 @@ const Bike_Categori = function () {
          </p>
          <p style={{
           margin: '5px',
-          fontSize: '10px',
+          fontSize: '13px',
           fontWeight: '500'
 
          }}>
@@ -239,7 +259,7 @@ const Bike_Categori = function () {
          height: '100%',
          background: 'white',
          display: 'flex',
-         justifyContent: 'space-around',
+         justifyContent: 'space-between',
          alignItems: 'center'
         }}>
          <div style={{
@@ -264,7 +284,7 @@ const Bike_Categori = function () {
           <p
            style={{
             margin: '5px',
-            fontSize: '10px',
+            fontSize: '13px',
             fontWeight: '500'
 
            }}
@@ -273,7 +293,7 @@ const Bike_Categori = function () {
           </p>
           <p style={{
            margin: '5px',
-           fontSize: '10px',
+           fontSize: '13px',
            fontWeight: '500'
 
           }}>
@@ -285,24 +305,30 @@ const Bike_Categori = function () {
           height: '90px',
 
           display: 'flex',
-          justifyContent: 'center',
+
           flexDirection: 'column'
          }}>
-          <img
-           style={{
-            width: '50%',
-            height: '50%',
-            objectFit: 'cover'
+          <div style={{
+           width: '50%',
+           height: '50%'
+          }}>
+
+           <img
+            style={{
+             width: '100%',
+             height: '100%',
+             objectFit: 'cover'
 
 
-           }}
-           src={water}
-           title='good'
-          />
+            }}
+            src={water}
+            title='good'
+           />
+          </div>
           <p
            style={{
             margin: '5px',
-            fontSize: '10px',
+            fontSize: '13px',
             fontWeight: '500'
 
            }}
@@ -311,7 +337,7 @@ const Bike_Categori = function () {
           </p>
           <p style={{
            margin: '5px',
-           fontSize: '10px',
+           fontSize: '13px',
            fontWeight: '500'
 
           }}>
@@ -329,6 +355,259 @@ const Bike_Categori = function () {
      </div>
 
 
+     <div style={{
+      width: '70%',
+      height: '100%',
+
+      display: 'flex',
+      justifyContent: 'space-between'
+     }}>
+
+
+      <div style={{
+       width: '45%',
+       height: '100%',
+       backgroundColor: 'white',
+       borderColor: 'black',
+       borderStyle: 'solid',
+       borderWidth: '1.5px',
+       display: 'flex',
+       flexDirection: 'column'
+      }}>
+       <div style={{
+        width: '100%',
+        height: '75%',
+
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+       }}>
+        <img
+         style={{
+          width: '100%',
+          height: '80%',
+          objectFit: 'cover'
+
+
+         }}
+         src={Bike}></img>
+       </div>
+       <div style={{
+        width: '100%',
+        height: '40%',
+
+        display: 'flex',
+        justifyContent: 'center'
+       }}>
+        <div style={{
+         width: '80%',
+         height: '100%',
+
+         display: 'flex',
+         flexDirection: 'column'
+        }}>
+         <p style={{
+          fontSize: '18px',
+          fontWeight: '600'
+         }}>
+          Himo Z20 Folding EBike
+         </p>
+         <p style={{
+          marginTop: '5px',
+          fontWeight: '600',
+          fontSize: '15px'
+
+         }}>
+          Color
+         </p>
+         <div style={{
+          display: 'flex',
+          flexDirection: 'row'
+         }}>
+
+
+          <div style={{
+           width: '50px',
+           height: '20px',
+           backgroundColor: 'black',
+           borderRadius: '20px'
+          }}>
+
+          </div>
+          <div style={{
+           width: '50px',
+           height: '20px',
+           backgroundColor: 'white'
+           ,
+           borderColor: 'black'
+           , borderStyle: 'solid',
+           borderWidth: '1px',
+           marginLeft: '20px',
+           borderRadius: '20px'
+          }}>
+
+          </div>
+
+
+         </div>
+         <div
+          style={{
+           display: 'flex',
+           alignContent: 'center',
+           justifyContent: 'flex-end'
+          }}
+         >
+          <p style={{
+           margin: 0,
+           fontSize: '13px',
+           fontWeight: '600',
+           marginRight: '10px'
+          }}> View Detail</p>
+          <AiOutlineRight></AiOutlineRight>
+         </div>
+
+        </div>
+
+       </div>
+
+      </div>
+
+
+
+
+
+
+
+      <div style={{
+       width: '45%',
+       height: '100%',
+       backgroundColor: 'black',
+       display: 'flex',
+       flexDirection: 'column'
+      }}>
+       <div style={{
+        width: '100%',
+        height: '75%',
+
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+       }}>
+        <img
+         style={{
+          width: '100%',
+          height: '80%',
+          objectFit: 'cover'
+
+
+         }}
+         src={Bike}></img>
+       </div>
+       <div style={{
+        width: '100%',
+        height: '40%',
+
+        display: 'flex',
+        justifyContent: 'center'
+       }}>
+        <div style={{
+         width: '80%',
+         height: '100%',
+
+         display: 'flex',
+         flexDirection: 'column'
+        }}>
+         <p style={{
+          fontSize: '18px',
+          fontWeight: '600',
+          color: 'white'
+         }}>
+          Himo Z20 Folding EBike
+         </p>
+         <p style={{
+          marginTop: '5px',
+          fontWeight: '600',
+          fontSize: '15px',
+          color: 'white'
+         }}>
+          Color
+         </p>
+         <div style={{
+          display: 'flex',
+          flexDirection: 'row'
+         }}>
+
+
+          <div style={{
+           width: '50px',
+           height: '20px',
+           backgroundColor: 'black',
+           borderRadius: '20px',
+           borderWidth: '1.5px',
+           borderStyle: 'solid'
+           , borderColor: 'white'
+          }}>
+
+          </div>
+          <div style={{
+           width: '50px',
+           height: '20px',
+           backgroundColor: 'white'
+           ,
+           borderColor: 'black'
+           , borderStyle: 'solid',
+           borderWidth: '1px',
+           marginLeft: '20px',
+           borderRadius: '20px'
+          }}>
+
+          </div>
+
+
+         </div>
+         <div
+          style={{
+           display: 'flex',
+           alignContent: 'center',
+           justifyContent: 'flex-end'
+          }}
+         >
+          <p style={{
+           margin: 0,
+           fontSize: '13px',
+           fontWeight: '600',
+           marginRight: '10px',
+           color: 'white'
+          }}> View Detail</p>
+          <AiOutlineRight
+           style={{ color: 'white' }}
+
+          ></AiOutlineRight>
+         </div>
+
+        </div>
+
+       </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+
+     </div>
+
+
+
 
 
     </div>
@@ -340,6 +619,7 @@ const Bike_Categori = function () {
     height: '60vh',
     backgroundColor: 'gold'
    }}>
+
 
    </div>
 
